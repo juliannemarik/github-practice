5f44f08a5352ff62ec5c79d424b44180b1db3411
Julianne Crawford <julianne.marik@gmail.com>
2020-07-05 21:34:16 -0500

    feat(repeat-function): add repeat function to code

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const helloWorld = (firstname, lastname) => {
   return `greetings ${firstname} ${lastname}`;
 }
 
-helloWorld("Testing", "123");
+helloWorld("Jane", "Doe");
+
+const repeat = (num, phrase) => {
+  for(let i = 0; i < num; i++) {
+    console.log(phrase);
+  }
+}
+
+repeat(10, 'hello there');
